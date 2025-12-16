60a2c53cb9bfae8bfe6187102ee4c0753d76efc4
std67122420213-NKR <std.67122420213@ubru.ac.th>
2025-12-16 11:46:52 +0700

    1st commit "Create Project"

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    title = 'Home Page'
+    return render_template('index.html',title='My Home Page')
+
+@app.route('/about')
+def about():
+    title = 'About Page'
+    name = 'Nakarin Chaleekeaw'
+    email = 'std.67122420213@ubru.ac.th'
+    mobile = '089-721xxxx'
+    age = 20
+    return render_template('about.html', title=title,
+                           name=name,
+                           email=email,
+                           mobile=mobile,
+                           age=age)
+
+@app.route('/favorite/foods')
+def favorite_foods():
+    title = 'Favorite Foods Page'
+    foods = ['ข้าวผัด','หมูกรอบ','บะหมี่']
+    return render_template('favorite_foods.html',
+                           title=title,
+                           foods=foods)
+
+@app.route('/favorite/sports')
+def favorite_sports():
+    title = 'Favorite Sports Page'
+    sports = ['ฟุตบอล','บาสเก็ตบอล','วอลเลห์บอล']
+    return render_template('favorite_sports.html',
+                           title=title,
+                           sports=sports)
